585a9117d10f19b8eefecbd5f8317f1af39694bf
Aniket Pal <67703407+Aniket762@users.noreply.github.com>
2020-07-21 16:02:21 +0530

    Operators (Video 5)
    This code shows the use of mathematical operators in python. It also shows how to use the math module.

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+
+n=int(input("Enter a number:"))
+m=int(input("Enter another number:"))
+
+print(m+n)
+print(m-n)
+print(m%n)
+print(m**n)
+print(m/n)
+print(m//n)
+
+print(m==n)
+
+print(math.exp(n))
